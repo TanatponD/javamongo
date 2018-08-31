4c9454c7384984b15431a25960d920d2cfb3963a
Taetanapat <tanapat_5365@hotmail.com>
2018-08-31 08:39:35 +0700

    before connect mongodb

--- src/Mongolabjava.java
+++ src/Mongolabjava.java
@@ -45,7 +45,34 @@ public class Mongolabjava {
         MongoClient client = new MongoClient(uri);
         MongoDatabase db = client.getDatabase(uri.getDatabase());
 
+        MongoCollection<Document> songs = db.getCollection("songs");
 
+        Document updateQuery = new Document("song", "One Sweet Day");
+        songs.updateOne(updateQuery, new Document("$set", new Document("artist", "Mariah Carey ft. Boyz II Men")));
+        Document findQuery = new Document("weeksAtOne", new Document("$gte",10));
+        Document orderBy = new Document("decade", 1);
 
+        MongoCursor<Document> cursor = songs.find(findQuery).sort(orderBy).iterator();
+
+        try {
+            while (cursor.hasNext()) {
+                Document doc = cursor.next();
+                System.out.println(
+                        "In the " + doc.get("decade") + ", " + doc.get("song") +
+                                " by " + doc.get("artist") + " topped the charts for " +
+                                doc.get("weeksAtOne") + " straight weeks."
+                );
+            }
+        } finally {
+            cursor.close();
+        }
+
+        // Since this is an example, we'll clean up after ourselves.
+
+        songs.drop();
+
+        // Only close the connection when your app is terminating
+
+        client.close();
     }
-}
+    }
